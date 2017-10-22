f2c6aa8e5ecacef415740913c5a01ed86ced6e7a
Roma <tera201@mail.ru>
2017-10-22 19:12:49 +0300

    +

---
+++ pack_3/3_3.py
@@ -0,0 +1,19 @@
+import math
+class Circle():
+    def __init__(self,r):
+        self.r=r
+    def P(self):
+        return 2*math.pi*self.r
+    def S(self):
+        return math.pi*(self.r**2)
+class Rectangle():
+    def __init__(self,l,w):
+        self.l=l
+        self.w=w
+    def S(self):
+        return self.l*self.w
+
+a=Circle(float(input("задайте радиус ")))
+print("Circle P=", a.P())
+print("Circle S=",a.S())
+print("Rectangle S=",Rectangle(float(input("задайте длину ")),float(input("задайте ширину "))).S())
