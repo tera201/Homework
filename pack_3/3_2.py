c200e4d4bb9549bd5a20315117448a126952c4d7
Roma <tera201@mail.ru>
2017-10-28 02:41:41 +0300

    +

--- pack_3/3_2.py
+++ pack_3/3_2.py
@@ -1,10 +1,14 @@
+ar = {1000: "M", 900: "CM", 500: "D", 400: "CD", 100: "C", 90: "XC", 50: "L", 40: "XL", 10: "X", 9: "IX", 5: "V",
+             4: "IV", 1: "I"}
 class tran:
     def RtoA(self,n):
         a=[]
         c=0
-        rm = {"I": 1, "V": 5, "X": 10, "L":50, "C":100, "D":500, "M":1000}
-        for i in n:
-            a.append(rm.get(i))
+        for p in n:
+            for i in ar:
+                if ar.get(i) == p:
+                    a.append(i)
+        print(a)
         for i in range(len(a)):
             if i>0 and a[i]>a[i-1]:
                 c+=a[i]-2*a[i-1]
@@ -14,8 +18,6 @@ class tran:
     
     def AtoR(self,n):
         a=""
-        ar = {1000: "M", 900: "CM", 500: "D", 400: "CD", 100: "C", 90: "XC", 50: "L", 40: "XL", 10: "X", 9: "IX", 5: "V",
-             4: "IV", 1: "I"}
         for i in ar:
             while i<=n:
                 a += ar[i]
